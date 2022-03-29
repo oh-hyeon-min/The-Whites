2c90cda8e291b1b81633f365535291b41e968695
oh-hyeon-min <dhgusals7777@naver.com>
2022-03-30 01:04:14 +0900

    기능 및 애니메이션 추가
    걷기, 달리기 기능 추가
    걷기, 달리기, 점프, 이단점프, 세이브포인트 애니메이션 추가
    세이브포인트 이미지 추가

--- Assets/System/Player.cs
+++ Assets/System/Player.cs
@@ -12,13 +12,12 @@ public class Player : MonoBehaviour
     public int Damage;
     public int CurrentHP;
     public int CurrentMP;
-    public float speed;
+    public float walkingSpeed;
+    public float runningSpeed;
     public float jumpPower;
-    bool isJumping = false;
-    bool isDoubleJumping = false;
     public float maxSpeed;
     private Rigidbody2D rigid;
-    BoxCollider2D colider;
+    public SpriteRenderer sprite;
     RaycastHit2D rayHit;
     public ScriptManager scriptManager;
     public GameObject savePanel;
@@ -27,29 +26,28 @@ public class Player : MonoBehaviour
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
-        colider = GetComponent<BoxCollider2D>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (anim.GetBool("Start") && !anim.GetBool("Die") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Character-Wake") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Character-Sleep"))
+        if (!savePanel.activeSelf && !scriptManager.isAction && anim.GetBool("Start") && !anim.GetBool("Die") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Character-Wake") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Character-Sleep"))
         {
-            if (Input.GetButtonDown("Jump") && !isJumping)
+            if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
             {
                 rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-                isJumping = true;
+                anim.SetBool("isJumping", true);
             }
-            else if (Input.GetButtonDown("Jump") && !isDoubleJumping)
+            else if (Input.GetButtonDown("Jump") && !anim.GetBool("isDoubleJumping"))
             {
                 rigid.velocity = new Vector2(rigid.velocity.x, 0);
                 rigid.AddForce(Vector2.up * jumpPower * 0.8f, ForceMode2D.Impulse);
-                isDoubleJumping = true;
+                anim.SetBool("isDoubleJumping", true);
             }
 
             if (rigid.velocity.y <= 0)
             {
+                anim.SetBool("Falling", true);
                 if (math.abs(rigid.velocity.y) >= maxSpeed)
                 {
                     rigid.velocity = new Vector2(rigid.velocity.x, -maxSpeed);
@@ -62,10 +60,14 @@ public class Player : MonoBehaviour
                 {
                     if (rayHit.distance < 2)
                     {
-                        isJumping = false;
-                        isDoubleJumping = false;
+                        anim.SetBool("isJumping", false);
+                        anim.SetBool("isDoubleJumping", false);
+                        anim.SetBool("Falling", false);
                     }
                 }
+            }
+            else {
+                anim.SetBool("Falling", false);
             }
         }
     }
@@ -81,19 +83,50 @@ public class Player : MonoBehaviour
             float fallSpeed = rigid.velocity.y; // 떨어지는 속도 저장
             if (inputX > 0)
             {
-                anim.SetBool("Walk", true);
+                if (!anim.GetBool("isJumping"))
+                {
+                    if (Input.GetKey(KeyCode.LeftShift))
+                    {
+                        anim.SetBool("Walk", true);
+                        anim.SetBool("Run", false);
+                    }
+                    else
+                    {
+                        anim.SetBool("Run", true);
+                        anim.SetBool("Walk", false);
+                    }
+                }
                 transform.localScale = new Vector3(-1, 1, 1);
             }
             else if (inputX < 0)
             {
-                anim.SetBool("Walk", true);
+                if (!anim.GetBool("isJumping"))
+                {
+                    if (Input.GetKey(KeyCode.LeftShift))
+                    {
+                        anim.SetBool("Walk", true);
+                        anim.SetBool("Run", false);
+                    }
+                    else
+                    {
+                        anim.SetBool("Run", true);
+                        anim.SetBool("Walk", false);
+                    }
+                }
                 transform.localScale = new Vector3(1, 1, 1);
             }
             else {
                 anim.SetBool("Walk", false);
+                anim.SetBool("Run", false);
             }
             Vector2 velocity = new Vector2(inputX, inputY);
-            velocity *= speed;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                velocity.x *= walkingSpeed;
+            }
+            else {
+                velocity.x *= runningSpeed;
+            }
             velocity.y = fallSpeed; // 떨어지는 속도 초기화
             rigid.velocity = velocity;
         }
